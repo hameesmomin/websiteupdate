4024aac4d903033b457652d97f14fc43c26f4bbe
Hamees <hamu.dxb@gmail.com>
2020-06-01 23:03:55 +0400

    First page update without edited footer

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,12 @@
 <!DOCTYPE html>
+
+
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+
 
         <title>Laravel</title>
 
@@ -18,7 +22,7 @@
                 left: 0;
                 top: 0;
 
-                background-image:linear-gradient(to bottom, #ed213a 0%, #263238 100%);
+
             }
             #one{
                 position: absolute;
@@ -37,6 +41,7 @@
                 line-height: 39px;
                 font-palette: dark;
                 /* identical to box height */
+                color: #FFFFFF;
             }
 
             #two{
@@ -53,6 +58,7 @@
                 font-weight: bold;
                 font-size: 16px;
                 line-height: 19px;
+                color: #FFFFFF;
             }
 
             #three{
@@ -72,7 +78,117 @@
                 /* identical to box height, or 100% */
 
                 text-align: center;
+                color: #FFFFFF;
             }
+
+            #four{
+                position: absolute;
+                width: 299px;
+                height: 32px;
+                left:41px;
+                top: 329px;
+                right:35px;
+                bottom:451px;
+
+                font-family: Dubai, serif;
+                font-style: normal;
+                font-weight: normal;
+                font-size: 18px;
+                line-height: 18px;
+                /* or 100% */
+
+                text-align: center;
+                color: #FFFFFF;
+
+            }
+
+            #five{
+                position: absolute;
+                width: 208px;
+                height: 18px;
+                left: 85px;
+                top: 456px;
+                right: 82px;
+                bottom: 338px;
+
+                font-family: Dubai, serif;
+                font-style: normal;
+                font-weight: normal;
+                font-size: 18px;
+                line-height: 18px;
+                /* identical to box height, or 100% */
+
+                display: flex;
+                align-items: center;
+                text-align: center;
+                color: #FFFFFF;
+
+            }
+
+            #six{
+                position: absolute;
+                width: 264px;
+                height: 48px;
+                left: 56px;
+                top: 615px;
+                bottom:149px;
+                right:55px;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+
+
+
+                background: #FFFFFF;
+                border-radius: 30px;
+            }
+
+            #even{
+                position: absolute;
+                width: 329px;
+                height: 84px;
+                left: 23px;
+                top: 379px;
+                bottom:350px;
+                right:23px;
+
+                font-family: Dubai, serif;
+                font-style: normal;
+                font-weight: 300;
+                font-size: 18px;
+                line-height: 18px;
+                /* or 100% */
+
+                display: flex;
+                align-items: center;
+                text-align: center;
+
+                color: #FFFFFF;
+            }
+
+            #seven{
+                position: absolute;
+                width: 375px;
+                height: 51px;
+                left: 0;
+                top: 737px;
+                bottom:24px;
+
+                background: linear-gradient(180deg, #F37335 0%, #FDC830 100%);
+            }
+            #eight{
+                position: absolute;
+                width: 375px;
+                height: 40px;
+                left: 0;
+                top: 772px;
+                background: rgba(243, 115, 53, 0.9);
+                transform: matrix(-1, 0, 0, 1, 0, 0);
+                
+
+                background: #FFFFFF;
+            }
+
             .full-height {
                 height: 100vh;
             }
@@ -117,7 +233,7 @@
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
+        <div class="flex-center position-ref full-height" style="width:375px; height:812px; background: linear-gradient(180deg, #ED213A 0%, #93291E 47.4%, #263238 100%);">
             <div id="one">
                FOUNDERS CLUB
             </div>
@@ -133,9 +249,26 @@
             </div>
             <br>
             <div id="four">
-
+                Innovation has the potential to change society
             </div>
             <br>
+            <div id="even">
+                This innovation will come from the people closest to technological change:
+            </div>
+            <br>
+            <div id="five">
+                Teenagers and Young Adults
+            </div>
+            <br>
+            <div id="six" >
+                <button class=" btn btn-outline-danger rounded-pill" style="height:48px; width:264px;border-radius: 25px; "> I'm ready for lift-off! </button>
+            </div>
+            <br>
+            <div id="seven">
+            </div>
+            <br>
+            <div id="eight">
+            </div>
         </div>
     </body>
 </html>
